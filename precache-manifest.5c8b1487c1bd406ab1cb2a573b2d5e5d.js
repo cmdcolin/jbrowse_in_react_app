ca3e1037340cfc3233331b5d7838e3f549cc1878
Colin <colin.diesh@gmail.com>
2020-08-14 20:00:11 -0400

    Updates

---
+++ precache-manifest.5c8b1487c1bd406ab1cb2a573b2d5e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161f7aa8be82b70d290340fdab34d615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbc7c86d7383270236da",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "311d64ec4144efbf2069",
+    "url": "/static/js/2.b26e108a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b26e108a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc7c86d7383270236da",
+    "url": "/static/js/main.bec14c80.chunk.js"
+  },
+  {
+    "revision": "2310f0ec3301195d6e49",
+    "url": "/static/js/runtime-main.438008f8.js"
+  }
+]);
